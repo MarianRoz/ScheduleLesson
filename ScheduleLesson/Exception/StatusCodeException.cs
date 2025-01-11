eb155ddcc04e7972d737a3bed53f2b42f294b4f5
DESKTOP-097O910\rozum <rozumnyi.marian@gmail.com>
2025-01-11 11:29:46 +0200

    Rename to StatusCodeException

---
+++ ScheduleLesson/Exception/StatusCodeException.cs
@@ -0,0 +1,13 @@
+﻿namespace ScheduleLesson
+{
+    public class StatusCodeException : Exception
+    {
+        public int StatusCode { get; }
+        public StatusCodeException(string message, int statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+
+
+    }
+}
