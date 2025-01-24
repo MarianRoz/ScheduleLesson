9343b9c5a3fe4547c2aafe3d1d5e70dfad58c7c1
DESKTOP-097O910\rozum <rozumnyi.marian@gmail.com>
2025-01-24 10:51:03 +0200

    Add userSersise

---
+++ ScheduleLesson/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScheduleLesson.Models;
+
+namespace ScheduleLesson.Services
+{
+    public interface IUserService
+    {
+        Task<ActionResult<IEnumerable<User>>> GetAllUsers();
+        Task<User> CreateUser(User user);
+        Task<User> GetUserById(int id);
+    }
+}
---
+++ ScheduleLesson/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScheduleLesson.Models;
+
+namespace ScheduleLesson.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApiDbContext _context;
+        public UserService(ApiDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        public async Task<User> GetUserById(int id)
+        {
+            User? user = await _context.Users.FindAsync(id);
+            return user;
+        }
+        public async Task<User> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return await GetUserById(user.Id);
+        }
+    }
+}
