99f4c66bd32718b60b955f28447ac677f467893f
DESKTOP-097O910\rozum <rozumnyi.marian@gmail.com>
2025-02-05 18:44:36 +0200

    Add user ID to all methods

--- ScheduleLesson/Services/IScheduleService.cs
+++ ScheduleLesson/Services/IScheduleService.cs
@@ -4,12 +4,11 @@ namespace ScheduleLesson.Services
 {
     public interface IScheduleService
     {
-        Task<List<Schedule>> GetAllSchedule();
-        Task<Schedule> GetSchedule(int id);
-        Task<Schedule> AddSchedule(Schedule schedule);
-        Task<Schedule> UpdateSchedule(Schedule schedule);
-        Task<Schedule> DeleteSchedule(int id);
-        Task<List<string>> GetDateTimeSchedule(DateTime dateTime);
-
+        Task<List<Schedule>> GetAllSchedule(string? userId);
+        Task<Schedule> GetScheduleById(int id, string? userId);
+        Task<Schedule> AddSchedule(Schedule schedule, string userId);
+        Task<Schedule> UpdateSchedule(Schedule schedule, string userId);
+        Task<Schedule> DeleteSchedule(int id, string userId);
+        Task<List<string>> GetDateTimeSchedule(DateTime dateTime, string userId);
     }
 }
