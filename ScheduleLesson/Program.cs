141dccdbdb349fdb9fc0506cfc9d86c22ae0cf9b
DESKTOP-097O910\rozum <rozumnyi.marian@gmail.com>
2025-02-06 22:54:57 +0200

    auth: Configure security in Program.cs

--- ScheduleLesson/Program.cs
+++ ScheduleLesson/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ScheduleLesson.Services;
 using Serilog;
 using Serilog.Sinks.MSSqlServer;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace ScheduleLesson
 {
@@ -14,31 +17,42 @@ namespace ScheduleLesson
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
             builder.Services.AddScoped<IScheduleService, ScheduleService>();
 
             builder.Services.AddDbContext<ApiDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+            builder.Services.AddAuthorization();
+            builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+               .AddEntityFrameworkStores<ApiDbContext>();
 
             // Рядок підключення до бази даних з appsettings.json
             string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
             // Налаштування Serilog
             Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(builder.Configuration) // Читає налаштування з appsettings.json
-                .WriteTo.Console() // Логування в консоль
-                .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day) // Логування у файл
-                .WriteTo.MSSqlServer(
-                    connectionString: connectionString,
-                    sinkOptions: new MSSqlServerSinkOptions
-                    {
-                        TableName = "Logs", // Назва таблиці для логів
-                        AutoCreateSqlTable = true // Автоматичне створення таблиці
-                    })
-                .CreateLogger();
+                 .ReadFrom.Configuration(builder.Configuration) // Читає налаштування з appsettings.json
+                 .WriteTo.Console() // Логування в консоль
+                 .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day) // Логування у файл
+                 .WriteTo.MSSqlServer(
+                     connectionString: connectionString,
+                     sinkOptions: new MSSqlServerSinkOptions
+                     {
+                         TableName = "Logs", // Назва таблиці для логів
+                         AutoCreateSqlTable = true // Автоматичне створення таблиці
+                     })
+                 .CreateLogger();
 
             // Додаємо Serilog до застосунку
             builder.Host.UseSerilog();
@@ -51,6 +65,8 @@ namespace ScheduleLesson
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.MapIdentityApi<IdentityUser>();
 
             app.UseMiddleware<ExceptionHandlingMiddleware>();
 
